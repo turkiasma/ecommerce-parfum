8dbac680de905691de89bc2ddb931c31bad637a4
turkiasma <asma.turki@medtech.tn>
2024-12-01 18:06:15 +0100

    changes in User and bag models

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,17 @@
 const express = require("express"); 
 const userRoute = require("./Routes/userRoute"); 
 const connectDb=require('./Configuration/connectDb') 
+var cors = require('cors') 
 const app = express(); 
 const dotenv = require("dotenv"); 
 dotenv.config(); 
 const port = process.env.PORT;
 connectDb(); 
-app.listen(port, (error) => { 
-if(error){console.log("Server Failed")} 
-else { 
+app.use(cors()) 
+app.listen(port, (er) => { 
+if (er) { 
+console.log(er); 
+} else { 
 console.log(`server is running on port ${port}`); 
 } 
 }); 
